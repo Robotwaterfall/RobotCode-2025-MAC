8285ebdc64dd88d39e61c070ad1c76ca1853a163
Jayson <jaysonwelshman@gmail.com>
2025-02-18 16:45:17 -0500

    Fixed the PID

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,49 +5,52 @@
 package frc.robot;
 
 import com.revrobotics.spark.SparkMax;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
 
-  private SparkMax PIDtestmotor = new SparkMax(13, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+  private SparkMax leftMotor1 = new SparkMax(13, MotorType.kBrushless);
 
-  
+  private Joystick joy1 = new Joystick(0);
 
-  private Joystick Joy1 = new Joystick(0);
-
-  private Encoder encoder = new Encoder(0,1 , false , Encoder.EncodingType.k4X);
+  private Encoder encoder = new Encoder(0, 1, false, EncodingType.k4X);
   private final double kDriveTick2Feet = 1.0 / 128 * 6 * Math.PI / 12;
 
-
-
+  @Override
+  public void robotInit() {
+  }
 
   @Override
   public void autonomousInit() {
     encoder.reset();
   }
 
-  final double kP = 0.05;
+  final double kP = 0.5;
 
   double setpoint = 0;
 
   @Override
   public void autonomousPeriodic() {
-    // get joystick command setting the setpoint
-    if (Joy1.getRawButton(1)) {
+    // get joystick command
+    if (joy1.getRawButton(1)) {
       setpoint = 10;
-    } else if (Joy1.getRawButton(2)) {
+    } else if (joy1.getRawButton(2)) {
       setpoint = 0;
     }
 
@@ -59,38 +62,29 @@ public class Robot extends TimedRobot {
 
     double outputSpeed = kP * error;
 
-    // set motor speed
-    PIDtestmotor.set(outputSpeed);
-
+    // output to motors
+    leftMotor1.set(outputSpeed);
   }
 
   @Override
   public void robotPeriodic() {
     SmartDashboard.putNumber("encoder value", encoder.get() * kDriveTick2Feet);
   }
- 
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
-  public void disabledInit() {}
+  public void testInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void testPeriodic() {
+  }
 
-  @Override
-  public void testInit() {}
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void simulationInit() {}
-
-  @Override
-  public void simulationPeriodic() {}
 }
