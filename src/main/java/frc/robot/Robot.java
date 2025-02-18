2a65bdeec15243102ed3a261e97945ffb0e8f538
Jayson <jaysonwelshman@gmail.com>
2025-02-18 11:47:44 -0500

    I have updated the robot's autonomous mode to support a broader range of actions.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -25,6 +27,7 @@ public class Robot extends TimedRobot {
    private TalonSRX LeftMasterMotor2 = new TalonSRX(3);
    private TalonSRX RightMasterMotor1 = new TalonSRX(4);
    private TalonSRX RightMasterMotor2 = new TalonSRX(1);
+   private SparkMax Testmotor = new SparkMax(13, MotorType.kBrushless);
 
   private Joystick joy1 = new Joystick(0);
 
@@ -43,7 +46,7 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     double time = Timer.getFPGATimestamp();
 
-    if (time - startTime < 3) {
+    if (time - startTime < 3) { // drive forward for 3 seconds
     LeftMasterMotor1.set(ControlMode.PercentOutput, 0.6);
     LeftMasterMotor2.set(ControlMode.PercentOutput, 0.6);
     RightMasterMotor1.set(ControlMode.PercentOutput, -0.6);
@@ -53,8 +56,37 @@ public class Robot extends TimedRobot {
       LeftMasterMotor2.set(ControlMode.PercentOutput, 0);
       RightMasterMotor1.set(ControlMode.PercentOutput, 0);
       RightMasterMotor2.set(ControlMode.PercentOutput, 0);
-    } 
-  }
+    }
+    
+    if (time - startTime > 3 && time - startTime < 5){ // turn right for 2 seconds at 60% speed
+    LeftMasterMotor1.set(ControlMode.PercentOutput, 0.6);
+    LeftMasterMotor2.set(ControlMode.PercentOutput, 0.6);
+    RightMasterMotor1.set(ControlMode.PercentOutput, 0);
+    RightMasterMotor2.set(ControlMode.PercentOutput, 0);
+    } else {
+      LeftMasterMotor1.set(ControlMode.PercentOutput, 0);
+      LeftMasterMotor2.set(ControlMode.PercentOutput, 0);
+      RightMasterMotor1.set(ControlMode.PercentOutput, 0);
+      RightMasterMotor2.set(ControlMode.PercentOutput, 0);
+    }
+    if (time - startTime > 5 && time -startTime < 10) { // drive backwards for 5 seconds at 60% speed
+      LeftMasterMotor1.set(ControlMode.PercentOutput, -0.6);
+      LeftMasterMotor2.set(ControlMode.PercentOutput, -0.6);
+      RightMasterMotor1.set(ControlMode.PercentOutput, 0.6);
+      RightMasterMotor2.set(ControlMode.PercentOutput, 0.6);
+    } else {
+      LeftMasterMotor1.set(ControlMode.PercentOutput, 0);
+      LeftMasterMotor2.set(ControlMode.PercentOutput, 0);
+      RightMasterMotor1.set(ControlMode.PercentOutput, 0);
+      RightMasterMotor2.set(ControlMode.PercentOutput, 0);
+    }
+    if (time - startTime > 10 && time - startTime < 12) { // turn testmotor for 2 seconds right at 50% speed
+      Testmotor.set(0.5);
+    } else {
+      Testmotor.set(0);
+    }
+
+}
  
 
   @Override
